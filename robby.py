e1313fc6dc833a84b8298274c86617aa3ab11511
Manas <manas.ks15@gmail.com>
2017-06-08 02:49:51 -0700

    experiments 1,2,3,4,5 done. Final commit. hopefully!

--- robby.py
+++ robby.py
@@ -19,16 +19,17 @@ no_of_steps=200
 reward=[-5,-1,10]
 actions=[0,1,2,3,4]   # actions = ['up', 'down', 'right', 'left', 'pick']
 dry_spell=0
-eta_array=[0.2,0.4,0.6,0.8,1]
-#epsilon_array=[0,0.25,0.5,0.75,1]
+eta_array=[0.2,0.4,0.6,0.8]
 
 
 UNIT=60     # pixels
 MAZE_H=10   # grid height
 MAZE_W=10   # grid width
 
+#Creating canvas for the grid
 canvas = tk.Canvas(bg='white',height=MAZE_H*UNIT,width=MAZE_W*UNIT)
 
+#Initializing the origin of the grid
 origin=numpy.array([20,20])
 
 def initialize_grid():
@@ -40,6 +41,7 @@ def initialize_grid():
         grid[numpy.arange(1,11), :] = numpy.random.choice([0, 1, 2], (10, 10), replace=True, p=[0.2, 0.4, 0.4])
     grid = numpy.concatenate((numpy.zeros((12, 1)), grid, numpy.zeros((12, 1))), axis=1)
     loc_R = tuple(list((numpy.random.choice(numpy.arange(1, 11), 2))))
+    #Creating 10x10 grid
     for c in range(0,MAZE_W*UNIT,UNIT):
         x0,y0,x1,y1=c,0,c,MAZE_H*UNIT
         canvas.create_line(x0,y0,x1,y1)
@@ -47,6 +49,7 @@ def initialize_grid():
         x0,y0,x1,y1=0,r,MAZE_H*UNIT,r
         canvas.create_line(x0,y0,x1,y1)
 
+    #Loading Can objects on the grid
     origin=numpy.array([20,20])
     for i in range(1,MAZE_H+1):
         for j in range(1,MAZE_W+1):
@@ -56,6 +59,7 @@ def initialize_grid():
 
     return grid, loc_R        
 
+#Compute current state of Robby based on sensor inputs
 def analyze_state(robby_loc,grid):
     base3=numpy.array([81, 27, 9, 3, 1])
     x,y=robby_loc
@@ -63,18 +67,19 @@ def analyze_state(robby_loc,grid):
     state=numpy.sum(base3*sensor_data)
     return state
 
+#Compute action to be performed by Robby based on QMatrix values for current state
 def select_action(qmatrix,state,epsilon):
     best_action = numpy.random.choice(numpy.nonzero(qmatrix[int(state),:]==numpy.amax(qmatrix[int(state),:]))[0], 1)
     action = numpy.random.choice([best_action,numpy.random.choice(5, 1)[0]],1,p=[(1-epsilon),epsilon]) 
     return action
 
+#Perform desired action (Left, Right, Up, Down, Pick)
 def commit_action(state,action,gamma,eta,step_cost,relocate,grid,loc_R,qmatrix,mode,t2,episode):
     if episode==0:
         time.sleep(0)
     if action==4:
         blank_center=origin+numpy.array([UNIT*(loc_R[0]-1),UNIT*(loc_R[1]-1)])
         blank=canvas.create_rectangle(blank_center[0]-5,blank_center[1]-5,blank_center[0]+25,blank_center[1]+25,fill='white')
-        #print "Can Location: ",loc_R,"Blank center: ",(blank_center-20)/60
         canvas.move(t2,0,0)
         canvas.update_idletasks()
         r=reward[int(grid[loc_R])] + step_cost
@@ -121,9 +126,9 @@ def commit_action(state,action,gamma,eta,step_cost,relocate,grid,loc_R,qmatrix,m
             if mode=='train':
                 qmatrix[int(state),action[0]]+=eta*(r+gamma*(numpy.amax(qmatrix[int(new_state),:]))-qmatrix[int(state),action[0]])
             state=new_state
-    #print "Location: ",loc_R,"Action: ",action,"T2 Coords: ",canvas.coords(t2)
     return state,grid,loc_R,qmatrix,r
 
+#Plotting function to plot rewards vs episodes
 def plot_rewards(episode_reward_list,num_episodes,exp,eta,epsilon,gamma):
     pyplot.figure(figsize=(12, 7.5), dpi=100)
     pyplot.plot(num_episodes, episode_reward_list, color='blue', label='Sum of Rewards per Episode' ,lw=2)
@@ -135,6 +140,7 @@ def plot_rewards(episode_reward_list,num_episodes,exp,eta,epsilon,gamma):
     pyplot.legend(loc='lower right')
     pyplot.savefig(exp)
 
+#Core Q-Learning Code
 def qlearn(qmatrix,mode,exp='Exp',gamma=0.9,eta=0.2,l_mode='const',step_cost=0,relocate=False,epsilon=1,e_mode='var',style='grid',action_r=None):
     global no_of_episodes
     episode_reward_array=[]
@@ -165,37 +171,35 @@ def qlearn(qmatrix,mode,exp='Exp',gamma=0.9,eta=0.2,l_mode='const',step_cost=0,r
             episode_reward_array.append(episode_reward) 
         
         canvas.delete("all")
-    #canvas.delete(ALL)
 
     if mode is 'train':
         num_episodes=numpy.concatenate((numpy.arange(0,no_of_episodes,100),[no_of_episodes-1]),axis=0)
         plot_rewards(episode_reward_array,num_episodes,exp,eta,epsilon,gamma)
     elif mode is 'test':
         print "\tTest Average: ",numpy.mean(episode_reward_array),"\tTest Standard Deviation: ",numpy.std(episode_reward_array)
-    
 
     return qmatrix
 
 def main():
     #Experiment 1
     print "Experiment 1"
-    #qmatrix=numpy.zeros((3**5,5))
-    #qmatrix=qlearn(qmatrix,mode='train',exp='Exp_1.png')
-    #qlearn(qmatrix,mode='test',epsilon=0,e_mode='const')
+    qmatrix=numpy.zeros((3**5,5))
+    qmatrix=qlearn(qmatrix,mode='train',exp='Exp_1.png')
+    qlearn(qmatrix,mode='test',epsilon=0,e_mode='const')
 
     #Experiment 2
     print "Experiment 2"
-    #for eta in eta_array:
-    #    qmatrix=numpy.zeros((3**5,5))
-    #    qmatrix=qlearn(qmatrix,mode='train',eta=eta,exp='Exp_2_eta_%2.1f.png'%eta)
-    #    qlearn(qmatrix,mode='test',epsilon=0,e_mode='const',eta=eta)
+    for eta in eta_array:
+        qmatrix=numpy.zeros((3**5,5))
+        qmatrix=qlearn(qmatrix,mode='train',eta=eta,exp='Exp_2_eta_%2.1f.png'%eta)
+        qlearn(qmatrix,mode='test',epsilon=0,e_mode='const',eta=eta)
 
     #Experiment 3
     print "Experiment 3"
-    #for epsilon in epsilon_array:
-        #qmatrix=numpy.zeros((3**5,5))
-        #qmatrix=qlearn(qmatrix,mode='train',epsilon=epsilon,exp='Exp_3_epsilon_%2.1f.png'%epsilon)
-        #qlearn(qmatrix,mode='test',epsilon=epsilon,e_mode='const')
+    epsilon=0.5
+    qmatrix=numpy.zeros((3**5,5))
+    qmatrix=qlearn(qmatrix,mode='train',epsilon=0.5,e_mode='const',exp='Exp_3_epsilon_%2.1f.png'%epsilon)
+    qlearn(qmatrix,mode='test',epsilon=0,e_mode='const')
 
     #Experiment 4
     print "Experiment 4"
@@ -204,12 +208,12 @@ def main():
     qlearn(qmatrix,mode='test',epsilon=0,e_mode='const',step_cost=-0.5)
 
     #Experiment 5
-    #print "Experiment 5"
+    print "Experiment 5"
+    qmatrix=numpy.zeros((3**5,5))
+    rewards=[-1,0,50]
+    qmatrix=qlearn(qmatrix,mode='train',epsilon=0.25,exp='Exp_5.png')
+    qlearn(qmatrix,mode='test',epsilon=0,e_mode='const')
 
-#if __name__ == "__main__":
-#    canvas.pack()
-#    canvas.after(0,main)
-#    canvas.mainloop()
 
 canvas.pack()
 canvas.after(0,main)

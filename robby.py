ba4302bedbfe7c33a8fa04cebbf9c25cc6c0ea15
Manas <manas.ks15@gmail.com>
2017-06-07 19:18:03 -0700

    experiment 1,2,3,4 done. Works with 20 episodes. need to run 5000 episodes

--- robby.py
+++ robby.py
@@ -19,6 +19,9 @@ no_of_steps=200
 reward=[-5,-1,10]
 actions=[0,1,2,3,4]   # actions = ['up', 'down', 'right', 'left', 'pick']
 dry_spell=0
+eta_array=[0.2,0.4,0.6,0.8,1]
+epsilon_array=[0,0.25,0.5,0.75,1]
+
 
 UNIT=60     # pixels
 MAZE_H=10   # grid height
@@ -65,8 +68,9 @@ def select_action(qmatrix,state,epsilon):
     action = numpy.random.choice([best_action,numpy.random.choice(5, 1)[0]],1,p=[(1-epsilon),epsilon]) 
     return action
 
-def commit_action(state,action,gamma,eta,step_cost,relocate,grid,loc_R,qmatrix,mode,t2):
-    #time.sleep(0.1)
+def commit_action(state,action,gamma,eta,step_cost,relocate,grid,loc_R,qmatrix,mode,t2,episode):
+    if episode==0:
+        time.sleep(0)
     if action==4:
         blank_center=origin+numpy.array([UNIT*(loc_R[0]-1),UNIT*(loc_R[1]-1)])
         blank=canvas.create_rectangle(blank_center[0]-5,blank_center[1]-5,blank_center[0]+25,blank_center[1]+25,fill='white')
@@ -120,20 +124,22 @@ def commit_action(state,action,gamma,eta,step_cost,relocate,grid,loc_R,qmatrix,m
     #print "Location: ",loc_R,"Action: ",action,"T2 Coords: ",canvas.coords(t2)
     return state,grid,loc_R,qmatrix,r
 
-def plot_rewards(episode_reward_list, num_episodes, exp):
+def plot_rewards(episode_reward_list,num_episodes,exp,eta,epsilon,gamma):
     pyplot.figure(figsize=(12, 7.5), dpi=100)
     pyplot.plot(num_episodes, episode_reward_list, color='blue', label='Sum of Rewards per Episode' ,lw=2)
     pyplot.xlabel('\nEpisodes\n', size=16)
     pyplot.ylabel('\nSum of Rewards\n', size=16)
-    pyplot.title('Q-Learning: Training Robby-The Robot\n', size=18)
+    pyplot.tick_params(axis='x', pad=30)
+    pyplot.xticks(range(0,no_of_episodes,100),rotation=(90),va="bottom",ha="left")
+    pyplot.title('Epsilon: %2.2f Eta: %2.2f Gamma: %2.2f\n'%(epsilon,eta,gamma), size=18)
     pyplot.legend(loc='lower right')
     pyplot.savefig(exp)
 
-def qlearn(qmatrix,mode,exp='Exp',gamma=0.9,eta=0.2,l_mode='const',step_cost=0,relocate=False,epsilon=1,e_mode='var',style='grid'):
+def qlearn(qmatrix,mode,exp='Exp',gamma=0.9,eta=0.2,l_mode='const',step_cost=0,relocate=False,epsilon=1,e_mode='var',style='grid',action_r=None):
     global no_of_episodes
     episode_reward_array=[]
     for episode in range(no_of_episodes):
-        sys.stdout.write("\rEpisode: %d"%episode)
+        sys.stdout.write("\rEpisode: %d   "%episode)
         sys.stdout.flush()
         terminator=itk.PhotoImage(Image.open("/home/manas/Projects/Namratha/MachineLearning/terminator.gif"))
         t2=canvas.create_image(30,30,image=terminator)
@@ -148,11 +154,12 @@ def qlearn(qmatrix,mode,exp='Exp',gamma=0.9,eta=0.2,l_mode='const',step_cost=0,r
         canvas.move(t2,UNIT*(robby_loc[0]-1),UNIT*(robby_loc[1]-1))
         episode_reward=0
         for step in range(no_of_steps):
-            #print "Step: ",step
             current_state=analyze_state(robby_loc,grid)
             action=select_action(qmatrix,current_state,epsilon)
-            current_state,grid,robby_loc,qmatrix,reward=commit_action(current_state,action,gamma,eta,step_cost,relocate,grid,robby_loc,qmatrix,"train",t2)
+            current_state,grid,robby_loc,qmatrix,reward=commit_action(current_state,action,gamma,eta,step_cost,relocate,grid,robby_loc,qmatrix,"train",t2,episode)
             episode_reward+=reward
+            if action_r is not None:
+                episode_reward+=-0.5
         
 	if mode is 'test'or(mode is 'train'and(episode%100==0 or episode==no_of_episodes-1)):
 	    episode_reward_array.append(episode_reward) 
@@ -162,7 +169,7 @@ def qlearn(qmatrix,mode,exp='Exp',gamma=0.9,eta=0.2,l_mode='const',step_cost=0,r
 
     if mode is 'train':
         num_episodes=numpy.concatenate((numpy.arange(0,no_of_episodes,100),[no_of_episodes-1]),axis=0)
-	plot_rewards(episode_reward_array,num_episodes,exp)
+	plot_rewards(episode_reward_array,num_episodes,exp,eta,epsilon,gamma)
     elif mode is 'test':
         print "\tTest Average: ",numpy.mean(episode_reward_array),"\tTest Standard Deviation: ",numpy.std(episode_reward_array)
     
@@ -174,19 +181,27 @@ def main():
     print "Experiment 1"
     qmatrix=numpy.zeros((3**5,5))
     qmatrix=qlearn(qmatrix,mode='train',exp='Exp_1.png')
-    qlearn(qmatrix,mode='test',epsilon=0.1,e_mode='const')
+    qlearn(qmatrix,mode='test',epsilon=0,e_mode='const')
 
     #Experiment 2
-    #print "Experiment 2"
-
+    print "Experiment 2"
+    for eta in eta_array:
+        qmatrix=numpy.zeros((3**5,5))
+        qmatrix=qlearn(qmatrix,mode='train',exp='Exp_2_eta_%2.1f.png'%eta)
+        qlearn(qmatrix,mode='test',eta=eta)
 
     #Experiment 3
-    #print "Experiment 3"
-
+    print "Experiment 3"
+    for epsilon in epsilon_array:
+        qmatrix=numpy.zeros((3**5,5))
+        qmatrix=qlearn(qmatrix,mode='train',exp='Exp_3_epsilon_%2.1f.png'%epsilon)
+        qlearn(qmatrix,mode='test',epsilon=epsilon,e_mode='const')
 
     #Experiment 4
-    #print "Experiment 4"
-
+    print "Experiment 4"
+    qmatrix=numpy.zeros((3**5,5))
+    qmatrix=qlearn(qmatrix,mode='train',exp='Exp_4.png')
+    qlearn(qmatrix,mode='test',epsilon=0,e_mode='const',action_r=True)
 
     #Experiment 5
     #print "Experiment 5"
